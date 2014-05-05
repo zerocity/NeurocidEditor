e6de9f6b5ed493081c9ea7b05e704d3e867876a6
Philpp Bisson <philipp.bisson@gmail.com>
2014-05-05 12:07:21 +0200

    go page

--- build.sh
+++
@@ -1,9 +0,0 @@
-#rm dist.nw && \
-grunt build && \
-cp package.json dist && \
-cp app/images/bp.png dist/images && \
-cp app/images/yeoman.png dist/images && \
-cd dist/ && \
-zip -r ../${PWD##*/}.nw * && \
-cd .. && \
-cat nw dist.nw > Neurocid && chmod +x Neurocid
